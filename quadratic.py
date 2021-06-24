514ac22bedb80b299b4e73a9db920e6b0a4a2c44
Stefan Ristic <85244232+risovac1@users.noreply.github.com>
2021-06-24 14:48:21 +0200

    Add files via upload

--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,19 @@
 import math
 
 def solve_quadratic(a,b,c):
-    x=(-b+math.sqrt(pow(b,2)-4*a*c))/2*a
-    y=(-b-math.sqrt(pow(b,2)-4*a*c))/2*a
-    if(x!=y):
-        print(x,y)
-    elif(x==y):
-        print(x)
+    z=pow(b,2)-4*a*c
+    if(z>=0):
+       
+        x=(-b+math.sqrt(z))/2*a
+        y=(-b-math.sqrt(z))/2*a
+        if(x!=y):
+            print(x,y)
+        elif(x==y):
+            print(x)
     else:
-        print('There is no solution')    
+        print('There is no result')    
 
 
 
 
-solve_quadratic(1,-5,6)   
+solve_quadratic(1,0,1)   
